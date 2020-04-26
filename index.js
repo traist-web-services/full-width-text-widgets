c45556c499ae75729b1cc61e5793cedc7744e405
Joe Innes <joe@joeinn.es>
2020-04-27 00:17:00 +0200

    Bugfixes

--- index.js
+++ index.js
@@ -14,22 +14,38 @@ module.exports = {
         def: false,
         label: "Uppercase?",
       },
+      {
+        type: 'singleton',
+        name: 'textContent',
+        contextualOnly: true
+      }
     ];
   },
   skipInitialModal: true,
   construct(self, options) {
-    const styles = options.fonts.map((font) => {
+    const styleDeclarationsArray = options.fonts.map((f, i) => {
+      // Create a class name based on the name and the index
+      // We can use this to create dynamic style blocks to
+      // feed to Tip Tap
+      return `.c-full-width-text__text--${f.split(' ')[0]}${i} {
+        font-family: '${f}'
+      }` 
+    });
+
+    const styles = options.fonts.map((font, i) => {
       return {
-        name: font,
-        element: "p", 
-        styles: {
-          'font-family': font
-        },
+        tag: 'span',
+        label: `${font}`,
+        'class': `c-full-width-text__text--${font.split(' ')[0]}${i}`
       };
     });
-    console.log(styles)
     self.addHelpers({
-      fontStyles: styles
+      fontStyles: [{
+          tag: 'p',
+          label: 'Default',        
+        },
+        ...styles],
+      styleDeclarations: styleDeclarationsArray.join('\n')
     });
     self.pushAsset("stylesheet", "widget");
     self.pushAsset("script", "fitty.min");
--- public/js/widget.js
+++ public/js/widget.js
@@ -1,21 +1,55 @@
-apos.on('enhance', resizeAllFullWidthText);
+apos.on('enhance', resizeAllFullWidthText)
+
+const fontList = styleInPage('fontFamily');
+// Get a list of fonts in use
+const fontLoadingPromises = fontList.filter(f => f !== 'inherit').map(font => document.fonts.load(`16px ${font}`));
+
+Promise.all(fontLoadingPromises)
+  .then(() => {
+    console.log('All fonts loaded')
+    if (!isFacebookApp()) {
+      resizeAllFullWidthText()
+    }
+  })
 
 function resizeAllFullWidthText () {
-  document.querySelectorAll(".fittext").forEach((el) => {
-    fitty(el, {
-      maxSize: 100,
-      minSize: 20
-    });
-  });
+  const options = {
+    minSize: 12,
+    maxSize: 300
+  }
+  document.querySelectorAll('.fittext').forEach(el => {
+    fitty(el, options)
+  })
+  fitty.fitAll()
 }
 
-(async function () {
-  await document.fonts.ready
-  fitty.fitAll({
-    maxSize: 100,
-    minSize: 20
-  })
-  setTimeout(resizeAllFullWidthText,500);
-})();
+if (!isFacebookApp()) {
+  resizeAllFullWidthText()
+}
 
-resizeAllFullWidthText();
+function styleInPage(css, verbose){
+  if(typeof getComputedStyle== "undefined")
+  getComputedStyle= function(elem){
+      return elem.currentStyle;
+  }
+  var who, hoo, values= [], val,
+  nodes= document.body.getElementsByTagName('*'),
+  L= nodes.length;
+  for(var i= 0; i<L; i++){
+      who= nodes[i];
+      if(who.style){
+          hoo= '#'+(who.id || who.nodeName+'('+i+')');
+          val= who.style.fontFamily || getComputedStyle(who, '')[css];
+          if(val){
+              if(verbose) values.push([hoo, val]);
+              else if(values.indexOf(val)== -1) values.push(val);
+          }
+      }
+  }
+  return values;
+}
+
+function isFacebookApp() {
+  var ua = navigator.userAgent || navigator.vendor || window.opera;
+  return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+}
